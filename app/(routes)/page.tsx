edff7c84bf6a5ed30ec82a822182faae85f5bf7a
Aliyu Efe <aliyuefe9501@gmail.com>
2024-04-21 23:28:24 +0100

    include product, image,size , color interfaces

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,6 +1,16 @@
-const Homepage = () => {
+import getBillboard from "@/actions/get-billboard"
+import Billboard from "@/components/ui/billboard"
+import Container from "@/components/ui/container"
+
+const Homepage = async () => {
+    const billboard = await getBillboard("65c009a231db1fa7d6dd4773")
+
     return (
-        <div>Hello Store</div>
+
+        <Container>
+            <div className="space-y-10 pb-10">Hello Store</div>
+            <Billboard data={billboard} />
+        </Container>
     )
 }
 
