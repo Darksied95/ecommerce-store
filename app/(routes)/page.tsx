207907a28692b34146eda0e1ba8e0ea0a8e0df22
Aliyu Efe <aliyuefe9501@gmail.com>
2024-04-23 12:21:35 +0100

    create get-product action

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,8 +1,11 @@
 import getBillboard from "@/actions/get-billboard"
+import getProducts from "@/actions/get-products"
+import ProductList from "@/components/product-list"
 import Billboard from "@/components/ui/billboard"
 import Container from "@/components/ui/container"
 
 const Homepage = async () => {
+    const products = await getProducts({ isFeatured: true })
     const billboard = await getBillboard("65c009a231db1fa7d6dd4773")
 
     return (
@@ -10,6 +13,9 @@ const Homepage = async () => {
         <Container>
             <div className="space-y-10 pb-10">Hello Store</div>
             <Billboard data={billboard} />
+            <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+                <ProductList title="Featured Products" items={products} />
+            </div>
         </Container>
     )
 }
