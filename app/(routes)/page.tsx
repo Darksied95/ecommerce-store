d7994f0b744a7f8a75dc4683f4e123977c5988da
Aliyu Efe <aliyuefe9501@gmail.com>
2024-04-27 00:35:17 +0100

    fix footer layout issue

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -11,10 +11,11 @@ const Homepage = async () => {
     return (
 
         <Container>
-            <div className="space-y-10 pb-10">Hello Store</div>
-            <Billboard data={billboard} />
-            <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-                <ProductList title="Featured Products" items={products} />
+            <div className="space-y-10 pb-10">
+                <Billboard data={billboard} />
+                <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+                    <ProductList title="Featured Products" items={products} />
+                </div>
             </div>
         </Container>
     )
